9b5422599232160a60b04326290343f752e285b3
LaurenBarer-SFSU <70980931+LaurenBarer-SFSU@users.noreply.github.com>
2020-10-19 14:39:37 -0700

    2020 CSC 317 myname.PHP

---
+++ myname2.php
@@ -0,0 +1,20 @@
+<img src=/sfsulogo.jpeg width=200>
+<br>
+<br>
+Hello, My name is Lauren Barer, Id-918330561, today is
+<br>
+<?php
+date_default_timezone_set('America/Los_Angeles');
+$stamp3 = date("m-d-Y h:i:s");
+print $stamp3;
+?>
+<br>
+<br>
+Here is my ID picture:
+<br>
+<img src=/IDSFSU.jpg width=200>
+<br>
+<br>
+And of coarse a little humor:
+<br>
+<img src=/meme.jpg width=300>
